3e5990bbe332d2d3c94a76db81aaceb9d3127f60
Rachit Mishra <rachitmishra5594@gmail.com>
2017-09-28 23:56:58 +0530

    Add files via upload

---
+++ Prog1b.java
@@ -0,0 +1,14 @@
+/* Write a Program to swap two numbers using the third variable*/
+
+class Prog1b{
+	public static void main(String[] args){
+		int a=6;
+		int b=5;
+		int c;
+		System.out.println("Before Swapping: a="+a+" and b="+b);
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("After Swapping: a="+a+" and b="+b);
+	}
+}
