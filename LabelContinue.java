39b9324c362336f5041953731264231c0c06c684
Rachit Mishra <rachitmishra5594@gmail.com>
2017-10-30 22:14:14 +0530

    Update and rename Prog9.java to LabelContinue.java

--- LabelContinue.java
+++ LabelContinue.java
@@ -1,6 +1,6 @@
 /* Write a program to display usage of break and continue statement */
 
-class Prog9{
+class LabelContinue{
 	public static void main(String args[]){
 		int a[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 		Loop1: 
